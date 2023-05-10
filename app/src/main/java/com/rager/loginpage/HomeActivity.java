f693b91aa97587a23438a55469df2479dbb0004b
divyansh-1902 <1902divyanshsingh@gmail.com>
2023-05-10 23:59:56 +0530

    home page activity

--- app/src/main/java/com/rager/loginpage/HomeActivity.java
+++ app/src/main/java/com/rager/loginpage/HomeActivity.java
@@ -3,6 +3,10 @@ package com.rager.loginpage;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class HomeActivity extends AppCompatActivity {
 
@@ -13,5 +17,16 @@ public class HomeActivity extends AppCompatActivity {
         if (getSupportActionBar() != null) {
             getSupportActionBar().hide();
         }
+
+        ImageView lefticon = findViewById(R.id.left_icon);
+        ImageView righticon = findViewById(R.id.right_icon);
+        TextView title = findViewById(R.id.toolbar_title);
+
+        lefticon.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(HomeActivity.this, "You clicked in left icon", Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
